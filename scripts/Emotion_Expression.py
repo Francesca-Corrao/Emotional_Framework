118a57d9b5e187543b88b61af53bf4b74daebf96
Francesca-Corrao <fra.corrao.99@gmail.com>
2024-04-09 16:17:54 +0200

    expression

--- scripts/Emotion_Expression.py
+++ scripts/Emotion_Expression.py
@@ -8,43 +8,45 @@ It takes into account the time decay of emotions and the fact that the transitio
 from sklearn.metrics.pairwise import cosine_similarity
 import time
 from naoqi import ALProxy
+import random
+import numpy as np 
+from numpy.linalg import norm
 #basic emotion in EPA
 emotion_map = {
-    "H":[3, 2.5, 2.8],
-    "N":[0,0,0],
-    "D":[-1,0,1],
-    "F":[-1.86, -2.2, 2.5],
-    "A":[-2, 1.5, 2],
-    "SA":[-3, -2.2 , 2.5],
-    "SU":[0, 0, 3]}
+    "H": np.array([3, 2.5, 2.8]),
+    "N":np.array([0,0,0]),
+    "F":np.array([-1.86, -2.2, 2.5]),
+    "A":np.array([-2, 1.5, 2]),
+    "SA":np.array([-3, -2.2 , -2.5]),
+    "SU":np.array([0, 0, 3])}
 
 happy_animation = [
-    "path1",
-    "path2",
-    "path3",
-    "path4",
-    "path5"]
+    "H:path1",
+    "H:path2",
+    "H:path3",
+    "H:path4",
+    "H:path5"]
 
 sad_animation = [
-    "path1",
-    "path2",
-    "path3",
-    "path4",
-    "path5"]
+    "S:path1",
+    "S:path2",
+    "S:path3",
+    "S:path4",
+    "S:path5"]
 
 angry_animation = [
-    "path1",
-    "path2",
-    "path3",
-    "path4",
-    "path5"]
+    "A:path1",
+    "A:path2",
+    "A:path3",
+    "A:path4",
+    "A:path5"]
 
 fear_animation = [
-    "path1",
-    "path2",
-    "path3",
-    "path4",
-    "path5"]
+    "F:path1",
+    "F:path2",
+    "F:path3",
+    "F:path4",
+    "F:path5"]
 
 IP_ADD = "10.0.0.2" #set correct IP 
 PORT = 9559 #set correct PORT 
@@ -54,12 +56,12 @@ class EmotionExpression():
     def __init__(self, type):
         self.emotion = [0,0,0]
         self.emo_label = "N"
-        self.time_decay = 4
+        self.time_decay = 2
         self.type = type
         if type == 'R':
             print("Robot Case")
-            self.animation_player = ALProxy("ALAnimationPlayerProxy", IP_ADD , PORT )
-            self.leds = ALProxy("ALLeds",IP_ADD,PORT)
+            #self.animation_player = ALProxy("ALAnimationPlayerProxy", IP_ADD , PORT )
+            #self.leds = ALProxy("ALLeds",IP_ADD,PORT)
         else:
             #facial expression module
             print("Avatar")
@@ -70,22 +72,28 @@ class EmotionExpression():
         cos_list = []
         #compute cosine similarity 
         for key in emotion_map:
-            cos_list.append(cosine_similarity(self.emotion, emotion_map[key]))
-        max_cos = 0
+            #cos_list.append(cosine_similarity(self.emotion.reshape(1,-1), emotion_map[key].reshape(1,-1)))
+            cos_list.append(np.dot(self.emotion,emotion_map[key])/(norm(self.emotion)*norm(emotion_map[key])))
+        print(cos_list)
+        max_cos = -1
         index = 0
         #get higher cosine similarity 
-        for i in len(cos_list):
-            if(cos_list[i]>max_cos):
+        for i in range(0, len(cos_list)):
+            if(cos_list[i] > max_cos):
+                print(str(i) + " maggiore di "+ str(index))
                 max_cos= cos_list[i]
                 index = i
-        
         #get label 
-        self.emo_label = emotion_map.key(index)
+        keys = list(emotion_map.keys())
+        print(emotion_map)
+        self.emo_label = keys[index]
+        print("basic emotion: "+ self.emo_label)
 
     def play_animation(self):
         #select random emotion
-        r= 0
-        animation, color
+        r= random.randint(0,4)
+        animation = "" 
+        color = ""
         if(self.emo_label == "H"):
             print("Happy")
             animation = happy_animation[r]
@@ -102,7 +110,8 @@ class EmotionExpression():
             print("Fear")
             animation= fear_animation[r]
             color = "purple"
-
+        print("color: "+ color)
+        print("animation: "+animation)
         #self.animation_player.run(animation,_async=True)
         #self.leds.fadeRGB("FaceLed", color, self.time_decay)
 
@@ -115,7 +124,6 @@ class EmotionExpression():
         if(self.type == "R"):
             if self.emo_label in ["H", "A", "F", "S"] :
                 print("play emotion:" + self.emo_label)
-                #python perchè non hai gli switch !!!!!
                 self.play_animation()
             elif(self.emo_label == "N"):
                 print("Neutral Emotional state")
@@ -129,8 +137,14 @@ class EmotionExpression():
          
 #main
 def main():
-    emo_exp = EmotionExpression()
+    emo_exp = EmotionExpression("R")
     while(True):
+        data = input("impression in EPA: ")
+        i = []
+        for val in data:
+            i.append(float(val))
+        emo_exp.emotion = np.array(i)
+        
         emo_exp.update_motion()
     
 
