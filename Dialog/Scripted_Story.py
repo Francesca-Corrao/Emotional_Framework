db610f622cfa1f2da4f54f1d7ee7bb04a9f69ce2
Francesca-Corrao <fra.corrao.99@gmail.com>
2024-05-23 15:42:35 +0200

    story

--- Dialog/Scripted_Story.py
+++ Dialog/Scripted_Story.py
@@ -1,7 +1,4 @@
 import requests
-#from dotenv import load_dotenv, find_dotenv
-import os
-#from openai import OpenAI
 import time
 import json
 from datetime import datetime
@@ -24,38 +21,33 @@ print(type(output_dic))
 while(1):
     print("start ", datetime.now())
     #robot_speech
-    agent_speech = output_dic['start'] + " How would you like to continue: " + output_dic["option_1"]["continue"] + " or " + output_dic["option_2"]["continue"] + " ? "
+    agent_speech = output_dic['start'] + " How would you like to continue: " + output_dic["option_1"]["continue"] + ", or " + output_dic["option_2"]["continue"] + " ? "
     #post agent_speech
     print("send robot speech")
     data = json.dumps(agent_speech)
-    #requests.post(url_ai+"/talk", json=data)
+    requests.post(url_ai+"/talk", json=data)
     #request user speech
     print("request user_speech")
     while(user_speech == None):
-        #data = requests.get(url_ic+"/audio_service").json()
+        data = requests.get(url_ic+"/audio_service").json()
         current = time.time()
         print(data)
-        user_speech = input("insert:")
-        """if(data['recognized']):
+        #user_speech = input("insert:")
+        if(data['recognized']):
             user_speech = data['text']
-            #send to sentimental analysis / perform sentimental analysis and send it to Emotion interface
-            #request emotional state
             print(user_speech)
         else:
             user_speech = None
-        """
-            
         if user_speech == "option one":
             print("option_1")
             update_dictionary(output_dic['option_1']["file"])
-
         elif user_speech == "option two":
             print("option_2")
             update_dictionary(output_dic['option_2']['file'])
         else:
             #no answer get wait for it
             user_speech = None 
-            agent_speech = "I am sorry I didn't get your choice. Can you please try againg slowly and louder"
+            agent_speech = "I am sorry I didn't get your choice. Can you please try again slowly and louder"
             data = json.dumps(agent_speech)
             requests.post(url_ai+"/talk", json=data)
     user_speech = None
--- scripts/Agent_Interface.py
+++ scripts/Agent_Interface.py
@@ -13,25 +13,29 @@ PORT = 9559 #set correct PORT
 PORT2 = 6001
 
 app = Flask(__name__)
-type = "A"
+type = "R"
 exp = ee.EmotionExpression(type= type)
 
 #pepper speaking Proxy
 if type == "R":
-    speak = ALProxy("ALAnimationPlayer", IP_ADD , PORT )
-
+    speak = ALProxy("ALAnimatedSpeech", IP_ADD , PORT )
+    sm = ALProxy("ALSpeakingMovement", IP_ADD, PORT)
+    #speak.setLanguage("English")
+    sm.setEnabled(True)
+    sm.setMode("contextual")
 @app.route("/talk", methods = ["POST"])
 def talk():
     data = request.get_json()
     speech = json.loads(data)
     print("Talking")
+    exp.get_emotion() #forse meglio mandarla su un thread vediamo
     print(speech)
     if type == "R":
         print(speech)
-        speak.say(speech)
-    time.sleep(30)
+        speak.say(str(speech))
+    #time.sleep(30)
     return jsonify(),200
 
-#threading.Thread(target=exp.main).start()
+threading.Thread(target=exp.main).start()
 
 app.run(host='127.0.0.1', port=PORT2)
